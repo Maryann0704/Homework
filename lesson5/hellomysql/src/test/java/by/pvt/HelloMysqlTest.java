5745bdb95dae9fc9e57e15b4af3fa0c054551367
Narushevich Marianna <marianna.naru@gmail.com>
2019-07-10 09:57:00 +0300

    create test database for lesson5

--- lesson5/hellomysql/src/test/java/by/pvt/HelloMysqlTest.java
+++ lesson5/hellomysql/src/test/java/by/pvt/HelloMysqlTest.java
@@ -1,31 +1,66 @@
 package by.pvt;
 
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
 import java.sql.*;
 
-public class HelloMysqlTest {
+public class HelloMysqlTest extends DBTestCase{
+
+    public HelloMysqlTest() {
+        System.setProperty(
+                PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS
+                , "com.mysql.jdbc.Driver" );
+        System.setProperty(
+                PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL
+                , "jdbc:mysql://localhost:3306/hello_mysql_junit" );
+        System.setProperty(
+                PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME
+                , "root" );
+        System.setProperty(
+                PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD
+                , "root" );
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(
+                HelloMysqlTest.class.getResourceAsStream("system_users.xml"));
+    }
 
     @Test
     public void testConnection() {
 
-        try {
-            Connection connection =
-                    DriverManager.getConnection(
-                            "jdbc:mysql://localhost:3306/hello_mysql"
-                            ,"root"
-                            ,"root");
+        try (
+                Connection connection =
+                        DriverManager.getConnection(
+                                "jdbc:mysql://localhost:3306/hello_mysql_junit"
+                                ,"root"
+                                ,"root");
 
-            PreparedStatement ps = connection.prepareStatement("select * from system_users");
+                PreparedStatement ps = connection.prepareStatement("select * from system_users");
 
-            ResultSet resultSet = ps.executeQuery();
+                ResultSet resultSet = ps.executeQuery();
+         ){
+            int rowCount = 0;
+            int activeUser = 0;
+            while (resultSet.next()){
+                rowCount++;
+                if (resultSet.getBoolean("active"))
+                    activeUser++;
+            }
+            Assert.assertEquals(4, rowCount);
+            Assert.assertEquals(2, activeUser);
 
-            assertNotNull(resultSet);
+            Assert.assertNotNull(resultSet);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
+
 }
