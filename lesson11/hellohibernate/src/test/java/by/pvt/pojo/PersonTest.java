c2d85cdf65663e6b7cef6b3c1b7f2878750997b4
Narushevich Marianna <marianna.naru@gmail.com>
2019-07-29 22:38:16 +0300

    lesson 12 test Person

--- lesson11/hellohibernate/src/test/java/by/pvt/pojo/PersonTest.java
+++ lesson11/hellohibernate/src/test/java/by/pvt/pojo/PersonTest.java
@@ -1,25 +1,62 @@
 package by.pvt.pojo;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import by.pvt.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class PersonTest {
 
     @Test
-    public void testPerson(){
+    public void testSavePerson() {
+        Transaction tx = null;
+        Session session = HibernateUtil.getInstance().getSession();
+        try {
+            tx = session.beginTransaction();
+            Person person = createTestData();
+            session.save(person);
+            tx.commit();
+            session.close();
 
-        SessionFactory sessionFactory =
-                new MetadataSources(
-                        new StandardServiceRegistryBuilder()
-                            .configure()
-                            .build()
-                ).buildMetadata().buildSessionFactory();
+            session = HibernateUtil.getInstance().getSession();
+            tx = session.beginTransaction();
+            List<Person> personList =
+                    session.createQuery("from person").list();
+            assertEquals(1, personList.size());
+            Person p2 = personList.get(0);
+            assertEquals(person, p2);
+            tx.commit();
+            session.close();
 
-        assertNotNull(sessionFactory);
+            session = HibernateUtil.getInstance().getSession();
+            tx = session.beginTransaction();
+            Person loadedPerson = session.get(Person.class, 1);
+            assertEquals(person, loadedPerson);
+            tx.commit();
+
+
+
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    private static Person createTestData() {
+        Person person = new Person();
+        person.setId(1);
+        person.setFirstName("FirstName");
+        person.setLastName("LastName");
+        person.setDateOfBirth(new Date());
+        person.setGender('f');
+        return person;
     }
 
 }
